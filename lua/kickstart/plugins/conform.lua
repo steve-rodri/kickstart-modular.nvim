c291f08cd279145737496a4e6bdd2ea5b60c7af2
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2024-07-11 19:06:29 -0400

    🐛 Fix prettier

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -3,14 +3,14 @@ return {
     'stevearc/conform.nvim',
     lazy = false,
     keys = {
-      -- {
-      --   '<leader>f',
-      --   function()
-      --     require('conform').format { async = true, lsp_fallback = true }
-      --   end,
-      --   mode = '',
-      --   desc = '[F]ormat buffer',
-      -- },
+      {
+        '<leader>F',
+        function()
+          require('conform').format { async = true, lsp_fallback = true }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
     },
     opts = {
       notify_on_error = true,
@@ -26,16 +26,12 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        javascript = { 'prettier' },
-        javascriptreact = { 'prettier' },
-        typescript = { 'prettier' },
-        typescriptreact = { 'prettier' },
-        markdown = { 'prettier' },
+        javascript = { { 'prettierd', 'prettier' } },
+        javascriptreact = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
+        typescriptreact = { { 'prettierd', 'prettier' } },
+        json = { { 'prettierd', 'prettier' } },
+        markdown = { { 'prettierd', 'prettier' } },
         sql = { 'sql-formatter' },
       },
     },
