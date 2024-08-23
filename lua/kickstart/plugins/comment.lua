4e8b3968838352fe875f7f75013c507a3c507602
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2024-08-23 14:24:00 -0400

    🐛 fix jsx commenting

--- lua/kickstart/plugins/comment.lua
+++ lua/kickstart/plugins/comment.lua
@@ -1,3 +1,13 @@
 return {
-  'numToStr/Comment.nvim', opts = {},
+  'numToStr/Comment.nvim',
+  config = function()
+    require('Comment').setup {
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+    }
+  end,
+  dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
+  opts = {},
 }
+
+-- Known issue with comments in jsx, temporarily solved with ts_context_commentstring above
+-- https://github.com/numToStr/Comment.nvim/pull/133
