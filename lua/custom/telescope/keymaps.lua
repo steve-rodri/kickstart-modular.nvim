a8709adaeb78194aaec1b4fd1a0d98959cd1a441
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2024-09-14 20:56:24 -0400

    ♻️ refator telescope keymaps

--- lua/custom/telescope/keymaps.lua
+++ lua/custom/telescope/keymaps.lua
@@ -1,22 +1,24 @@
 -- See `:help telescope.builtin`
 local builtin = require 'telescope.builtin'
+
 vim.keymap.set('n', '<leader>f', function()
   builtin.find_files { hidden = true }
-end, { desc = '[F]ind File' })
-vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-vim.keymap.set('n', '<leader>sf', builtin.git_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+end, { desc = '[f]ind file' })
 
-vim.keymap.set('n', '<leader>sm', '<cmd>Telescope media_files<cr>', { desc = '[S]earch [M]edia' })
-vim.keymap.set('n', '<leader>se', '<cmd>Telescope emoji<cr>', { desc = '[S]earch [E]moji' })
-vim.keymap.set('n', '<leader>sD', '<cmd>Telescope dap<cr>', { desc = '[S]earch [D]ap' })
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[h]elp' })
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[k]eymaps' })
+vim.keymap.set('n', '<leader>sf', builtin.git_files, { desc = '[f]iles (git)' })
+vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[s]elect Telescope' })
+vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = 'current [w]ord' })
+vim.keymap.set('n', '<leader>st', builtin.live_grep, { desc = '[t]ext' })
+vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[d]iagnostics' })
+vim.keymap.set('n', '<leader>sr', builtin.oldfiles, { desc = '[r]ecent files' })
+vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[b]uffers' })
+vim.keymap.set('n', '<leader><leader>', builtin.resume, { desc = 'Resume Telescope Search' })
+
+vim.keymap.set('n', '<leader>sm', '<cmd>Telescope media_files<cr>', { desc = '[m]edia' })
+vim.keymap.set('n', '<leader>se', '<cmd>Telescope emoji<cr>', { desc = '[e]moji' })
+vim.keymap.set('n', '<leader>sD', '<cmd>Telescope dap<cr>', { desc = '[D]ap' })
 
 -- Slightly advanced example of overriding default behavior and theme
 vim.keymap.set('n', '<leader>/', function()
@@ -34,9 +36,9 @@ vim.keymap.set('n', '<leader>s/', function()
     grep_open_files = true,
     prompt_title = 'Live Grep in Open Files',
   }
-end, { desc = '[S]earch [/] in Open Files' })
+end, { desc = '[/] in Open Files' })
 
 -- Shortcut for searching your Neovim configuration files
 vim.keymap.set('n', '<leader>sc', function()
   builtin.find_files { cwd = vim.fn.stdpath 'config' }
-end, { desc = '[S]earch [C]onfiguration files' })
+end, { desc = '[c]onfiguration files' })
