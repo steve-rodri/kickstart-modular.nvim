c1eed3170c3358a57d99442714224189efee6d99
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2024-09-15 18:56:23 -0400

    🔧 Add close other buffers keymap

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -6,12 +6,14 @@ return {
     require('bufferline').setup {}
     vim.opt.termguicolors = true
 
-    vim.keymap.set('n', '<leader>c', '<cmd>bdelete<cr>', { desc = '[C]lose current buffer' })
+    vim.keymap.set('n', '<leader>c', '<cmd>bdelete<cr>', { desc = '[c]lose current buffer' })
     vim.keymap.set('n', '<leader>C', '<cmd>bdelete!<cr>', { desc = 'Force [C]lose current buffer' })
 
     vim.keymap.set('n', '<S-h>', '<cmd>BufferLineCyclePrev<cr>', { desc = 'Previous buffer' })
     vim.keymap.set('n', '<S-l>', '<cmd>BufferLineCycleNext<cr>', { desc = 'Next buffer' })
-    vim.keymap.set('n', '<leader>bh', '<cmd>BufferLineCloseLeft<cr>', { desc = 'Close all buffers to the left' })
-    vim.keymap.set('n', '<leader>bl', '<cmd>BufferLineCloseRight<cr>', { desc = 'Close all buffers to the right' })
+
+    vim.keymap.set('n', '<leader>bh', '<cmd>BufferLineCloseLeft<cr>', { desc = 'Close all buffers to the [l]eft' })
+    vim.keymap.set('n', '<leader>bl', '<cmd>BufferLineCloseRight<cr>', { desc = 'Close all buffers to the [r]ight' })
+    vim.keymap.set('n', '<leader>bo', '<cmd>BufferLineCloseOthers<cr>', { desc = 'Close all [o]ther buffers' })
   end,
 }
