97f8e14c8d6234bee27274526c89df470693b0d7
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2025-06-03 14:28:06 +0700

    fix avante.nvim depecration warnings

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -2,32 +2,31 @@ return {
   'yetone/avante.nvim',
   event = 'VeryLazy',
   version = false, -- Never set this value to "*"! Never!
-  opts = {
-    provider = 'openai',
+
+  providers = {
     openai = {
       endpoint = 'https://api.openai.com/v1',
-      model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
+      model = 'gpt-4.1', -- your desired model (or use gpt-4o, etc.)
       timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
       temperature = 0,
       max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
       --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
     },
+    groq = { -- define groq provider
+      __inherited_from = 'openai',
+      api_key_name = 'GROQ_API_KEY',
+      endpoint = 'https://api.groq.com/openai/v1/',
+      model = 'llama-3.3-70b-versatile',
+      max_completion_tokens = 32768, -- remember to increase this value, otherwise it will stop generating halfway
+    },
+  },
 
+  opts = {
     --- ... existing configurations
     cursor_applying_provider = 'groq', -- In this example, use Groq for applying, but you can also use any provider you want.
     behaviour = {
       --- ... existing behaviours
       enable_cursor_planning_mode = true, -- enable cursor planning mode!
-    },
-    vendors = {
-      --- ... existing vendors
-      groq = { -- define groq provider
-        __inherited_from = 'openai',
-        api_key_name = 'GROQ_API_KEY',
-        endpoint = 'https://api.groq.com/openai/v1/',
-        model = 'llama-3.3-70b-versatile',
-        max_completion_tokens = 32768, -- remember to increase this value, otherwise it will stop generating halfway
-      },
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
