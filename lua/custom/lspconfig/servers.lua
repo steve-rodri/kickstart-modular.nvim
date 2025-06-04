cf0d2067b3944d257f812c5b2bbc5cf1308e0438
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2025-06-04 16:44:48 +0700

    clean up lspconfig

--- lua/custom/lspconfig/servers.lua
+++ lua/custom/lspconfig/servers.lua
@@ -1,9 +1,6 @@
 local M = {}
 
 M.setup = function()
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-
   require('mason').setup()
 
   require('mason-tool-installer').setup {
@@ -14,15 +11,8 @@ M.setup = function()
   }
 
   require('mason-lspconfig').setup {
-    handlers = {
-      function(server_name)
-        local status, server = pcall(require, 'custom.lspconfig.servers.' .. server_name)
-        if not status then
-          server = {}
-        end
-        server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-        require('lspconfig')[server_name].setup(server)
-      end,
+    ensure_installed = {
+      'lua_ls',
     },
   }
 end
